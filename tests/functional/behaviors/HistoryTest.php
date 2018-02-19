428bd073dba10c97b56d437034d95a243caee7f9
Vasily Belosludcev <bupy765@gmail.com>
2018-02-20 02:14:23 +0500

    adding tests

---
+++ tests/functional/behaviors/HistoryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace bupy7\activerecord\history\tests\functionals\behaviors;
+
+use Yii;
+use bupy7\activerecord\history\tests\functionals\TestCase;
+
+class CropboxTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->mockApplication();
+    }
+
+    public function testAssets()
+    {
+
+    }
+}
